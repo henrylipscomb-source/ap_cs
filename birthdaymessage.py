5ad6da154eb58aa1bcecd0c7e332890d9143c2d3
HenryLipscomb <HL@mail.com>
2026-02-12 14:41:43 -0500

    Python file

---
+++ birthdaymessage.py
@@ -0,0 +1,5 @@
+name = "Georgie"
+print(f"Happy Birthday to {name}")
+
+#python doesn't need to compile, functions are not required (or optional), 
+#dynamic data typing, much bigger standard library.
